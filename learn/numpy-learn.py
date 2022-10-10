c78debd331ba997b33926bc5f824d167ff01886d
owen920831 <owen920831@gmail.com>
2022-10-10 22:27:49 +0800

    done!

--- learn/numpy-learn.py
+++ learn/numpy-learn.py
@@ -1,6 +1,6 @@
-import numpy as np;
-
-
-data_x = np.power(2.3, np.arange(10))
+import math
+data_x = 0
+for n in range(21, 121):
+    data_x += math.comb(120, n)*math.pow(0.1, n)*math.pow(0.9, 120-n)
 
 print(data_x)
