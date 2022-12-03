e8eea92c8d32073edec724c9a97bd0457b1392a2
owen920831 <owen920831@gmail.com>
2022-12-03 15:47:30 +0800

    1

--- hw4/Loss.py
+++ hw4/Loss.py
@@ -24,5 +24,30 @@ def compute_BCE_cost(AL, Y):
     assert(cost.shape == ())
     
     return cost
+# GRADED FUNCTION: compute_CCE_cost
 
+def compute_CCE_cost(AL, Y):
+    """
+    Implement the categorical cross-entropy cost function using the above formula.
+
+    Arguments:
+    AL -- probability vector corresponding to your label predictions, shape (number of classes, number of examples)
+    Y -- true "label" vector (one hot vector, for example: [[1], [0], [0]] represents rock, [[0], [1], [0]] represents paper, [[0], [0], [1]] represents scissors 
+                              in a Rock-Paper-Scissors image classification), shape (number of classes, number of examples)
+
+    Returns:
+    cost -- categorical cross-entropy cost
+    """
+    
+    m = Y.shape[1]
+
+    # Compute loss from aL and y.
+    ### START CODE HERE ### (≈ 1 line of code)
+    cost = -1/m * np.sum(Y*np.log(AL+1e-5))
+    ### END CODE HERE ###
+    
+    cost = np.squeeze(cost)      # To make sure your cost's shape is what we expect (e.g. this turns [[17]] into 17).
+    assert(cost.shape == ())
+    
+    return cost
 
